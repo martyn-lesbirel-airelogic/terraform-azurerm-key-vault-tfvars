6cbefa16b6f4c0b6634b23dae86c11b16d067573
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-03-07 14:02:16 +0000

    Added azuread provider version

--- versions.tf
+++ versions.tf
@@ -1,9 +1,15 @@
 terraform {
   required_version = ">= 1.3.9"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.41.0"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.3.6"
     }
   }
 }
